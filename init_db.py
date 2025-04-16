7f4cfba9e3f4490ae428cb7ae5a0011dfb6a71d1
Aikelen <124249918+Aikelen@users.noreply.github.com>
2025-04-17 02:57:25 +0300

    Add files via upload

---
+++ init_db.py
@@ -0,0 +1,16 @@
+import asyncio
+import asyncpg
+import os
+
+async def create_table():
+    conn = await asyncpg.connect(os.getenv("DATABASE_URL"))
+    await conn.execute("""
+        CREATE TABLE IF NOT EXISTS scores (
+            user_id BIGINT PRIMARY KEY,
+            username TEXT,
+            score INTEGER
+        );
+    """)
+    await conn.close()
+
+asyncio.run(create_table())
